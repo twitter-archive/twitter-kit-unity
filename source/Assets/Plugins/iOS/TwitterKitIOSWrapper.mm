da19732e063396d9fb28a16008ac16bb284abc67
Gabor Vass <gabor@gramble.com>
2017-08-24 11:02:35 -0700

    Ensure Login view is presented on top of the rootViewController
    Differential Revision: https://phabricator.twitter.biz/D84407

--- source/Assets/Plugins/iOS/TwitterKitIOSWrapper.mm
+++ source/Assets/Plugins/iOS/TwitterKitIOSWrapper.mm
@@ -151,8 +151,9 @@ void TwitterInit(const char *consumerKey, const char *consumerSecret)
  */
 void TwitterLogIn()
 {
-   [[Twitter sharedInstance] logInWithCompletion:^(TWTRSession *session, NSError *error) {
-       if (session) {
+    UIViewController *rootViewController = [[[UIApplication sharedApplication] keyWindow] rootViewController];
+    [[Twitter sharedInstance] logInWithViewController:rootViewController completion:^(TWTRSession *session, NSError *error) {
+        if (session) {
             NSDictionary *sessionDictionary = NSDictionaryFromTWTRSession(session);
             char *serializedSession = serializedJSONFromNSDictionary(sessionDictionary);
             UnitySendMessage(TWTRInternalGameObject, TWTRUnityAPIMethodLogInComplete, serializedSession);
